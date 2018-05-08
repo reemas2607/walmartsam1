6e8073eb6119a828bf065aa34647ae55de15daba
reemas2607 <39100292+reemas2607@users.noreply.github.com>
2018-05-09 00:18:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.walmart.com/ip/Double-Heart-Love-White-CZ-Promise-Ring-Sizes-5-6-7-8-9-10-New-925-Sterling-Silver-Band-Rings-by-Sac-Silver-Size-7/138265863");
 die();
 ?>
