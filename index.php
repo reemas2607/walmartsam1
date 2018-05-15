1f4da42869099ab53b45df818b087ac239e9813b
reemas2607 <39100292+reemas2607@users.noreply.github.com>
2018-05-15 07:07:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/ip/Double-Heart-Love-White-CZ-Promise-Ring-Sizes-5-6-7-8-9-10-New-925-Sterling-Silver-Band-Rings-by-Sac-Silver-Size-7/138265863");
+header("Location: http://www.uniquehimachal.co.in/");
 die();
 ?>
